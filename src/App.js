bf0a956903c7749551b57e0c2894a501ee1990df
Areeza Asrhad <arshadareeza310@gmail.com>
2025-08-26 17:19:21 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Quote from './Components/Quote-Generator/Quote';
+
+
+function App() {
+  return (
+    <div>
+     <Quote/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Quote-Generator/Quote.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './Quote.css';
+import reload_icon from './Assets/reload.png';
+import twt_icon from './Assets/twt.png';
+const apiUrl = "https://api.adviceslip.com/advice";
+
+const Quote = () => {
+    const [quote, setQuote] = useState('dont judge a book by its cover');
+    const quoteGenerator = async () => {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setQuote(data.slip.advice)
+    }
+  return (
+    <div>
+      <div className="container">
+        <div className="quote">{quote}</div>
+        <div className="line"></div>
+        <div className="content">
+          <div className="author">~unknown</div>
+          <div className="img-container">
+            <img src={reload_icon} alt="reload" onClick={quoteGenerator}/>
+            <img src={twt_icon} alt="tweet" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Quote;
